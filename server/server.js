a5bba424b0ca6837d8d152195e158178afad6995
govindhansv <govindhansv333@gmail.com>
2021-12-27 16:33:59 +0530

    project structure created

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const PORT = 8000;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('Welcome to Backend! Bitch')
+})
+
+var indexRouter = require('./routes/index');
+
+app.use('index',indexRouter);
+
+
+app.listen(PORT,()=>{
+    console.log(`server is listening on ${PORT}`);
+})
