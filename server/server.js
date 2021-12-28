f363b4e5ed407b3cd29bb4fd148b80c052b321f2
govindhansv <govindhansv333@gmail.com>
2021-12-28 12:26:53 +0530

    session setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const PORT = 8000;
+
 const express = require('express');
 const app = express();
+
+
 const cors = require('cors');
+const sessions = require('express-session');
 var db = require('./connection');
 
 // git controller and tester....
@@ -9,7 +17,12 @@ var db = require('./connection');
 merging by kannappan
 */
 // ends 
+
+// middlewares 
 app.use(cors());
+
+
+// getting and posting methods
 
 app.get('/',(req,res)=>{
     res.send('Welcome to Backend! Bitch ')
@@ -20,14 +33,31 @@ app.post("/post", (req, res) => {
   res.redirect("/");
 });
 
+
+// router setting
 var indexRouter = require('./routes/index');
 app.use('/index',indexRouter);
 
+//  session setup
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+
+//session middleware
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
+
+
+// db connection
 db.connect((err)=>{
   if(err) console.log("Connection Error"+err);
   else console.log("Database connected to port")
 })
 
+// server listening
 app.listen(PORT,()=>{
     console.log(`server is listening on ${PORT}`);
 })
