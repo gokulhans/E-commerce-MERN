980b5f4dc05207add1b6fca2b781b065b83c3955
Gokul Hans V <gokulhansv@gmail.com>
2022-01-16 13:33:50 +0530

    landing style

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -1,45 +1,65 @@
-import React,{useState} from 'react'
-import {useNavigate} from 'react-router-dom'
-
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import './AddProduct.css'
 
 const AddProduct = () => {
 
     const navigate = useNavigate()
 
-   
+
     const [item, setitem] = useState('');
     const [price, setprice] = useState('');
     const [type, settype] = useState('');
     const [des, setdes] = useState('');
 
 
-    const PostData = ()=>{ 
+    const PostData = () => {
         console.log('çlicked');
-        fetch("products/add-product",{
-            method:"post",
-            headers:{
-                "Content-Type":"application/json"
+        fetch("products/add-product", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
             },
-            body:JSON.stringify({
+            body: JSON.stringify({
                 type,
-                item,price,des
+                item, price, des
             })
-        }).then(response=>response.json())
-        .then(data=>{
-            console.log(data);
-            navigate('/')
-        })
+        }).then(response => response.json())
+            .then(data => {
+                console.log(data);
+                navigate('/')
+            })
     }
 
 
 
     return (
-        <div>
-            <input type="text" placeholder='item' onChange={(e)=>setitem(e.target.value)} />
-            <input type="text" placeholder='type' onChange={(e)=>settype(e.target.value)} />
-            <input type="text" placeholder='description' onChange={(e)=>setdes(e.target.value)} />
-            <input type="text" placeholder='price' onChange={(e)=>setprice(e.target.value)} />
-            <input type="button" value="Add Product" onClick={()=>PostData()} />
+
+        <div className="main">
+            <div className="addproduct">
+                <h3>Add Product</h3>
+                <div>
+
+                    <div class="form mb-2">
+                        <label for="form5Example1">Item</label>
+                        <input type="text" id="form5Example1" class="form-control" onChange={(e) => setitem(e.target.value)} />
+                    </div>
+                    <div class="form mb-2">
+                        <label for="form5Example2">Description</label>
+                        <input type="text" id="form5Example2" class="form-control" onChange={(e) => setdes(e.target.value)} />
+                    </div>
+                    <div class="form mb-2">
+                        <label for="form5Example2">Description</label>
+                        <input type="text" id="form5Example2" class="form-control" onChange={(e) => setprice(e.target.value)} />
+                    </div>
+                    <div class="form d-flex justify-content-center mb-4">
+                        <label class="form-label">
+                            click add button to add products in your e-shope
+                        </label>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block mb-4" onClick={() => PostData()} >Add</button>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,12 +6,121 @@ const Home = () => {
         <>
             <div className="main">
                 <div className="head">
-                <h1 className="head1">TRENDS</h1>
-                <h3 className="head2">Where The World Find Their Fashion</h3>
+                    <h1 className="head1">TRENDS</h1>
+                    <h3 className="head2">Where The World Find Their Fashion</h3>
                 </div>
                 <img className="image" src="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
-                <img className="image" src="https://images.unsplash.com/photo-1558191053-8edcb01e1da3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" ></img>
-                <img className="image" src="https://images.unsplash.com/photo-1445205170230-053b83016050?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" ></img>
+            <h1 className="sub-head">Shop Now</h1>
+            </div>
+
+
+            <div class="container bg-white">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
+                        <div class="product"> <img src="https://images.pexels.com/photos/54203/pexels-photo-54203.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+                            <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
+                                <li class="icon"><span class="fas fa-expand-arrows-alt"></span></li>
+                                <li class="icon mx-3"><span class="far fa-heart"></span></li>
+                                <li class="icon"><span class="fas fa-shopping-bag"></span></li>
+                            </ul>
+                        </div>
+                        <div class="tag bg-red">sale</div>
+                        <div class="title pt-4 pb-1">Winter Sweater</div>
+                        <div class="d-flex align-content-center justify-content-center"> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> </div>
+                        <div class="price">$ 60.0</div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
+                        <div class="product"> <img src="https://images.pexels.com/photos/6764040/pexels-photo-6764040.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+                            <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
+                                <li class="icon"><span class="fas fa-expand-arrows-alt"></span></li>
+                                <li class="icon mx-3"><span class="far fa-heart"></span></li>
+                                <li class="icon"><span class="fas fa-shopping-bag"></span></li>
+                            </ul>
+                        </div>
+                        <div class="tag bg-black">out of stock</div>
+                        <div class="title pt-4 pb-1">Denim Dresses</div>
+                        <div class="d-flex align-content-center justify-content-center"> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> </div>
+                        <div class="price">$ 55.0</div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
+                        <div class="product"> <img src="https://images.pexels.com/photos/914668/pexels-photo-914668.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+                            <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
+                                <li class="icon"><span class="fas fa-expand-arrows-alt"></span></li>
+                                <li class="icon mx-3"><span class="far fa-heart"></span></li>
+                                <li class="icon"><span class="fas fa-shopping-bag"></span></li>
+                            </ul>
+                        </div>
+                        <div class="tag bg-green">new</div>
+                        <div class="title pt-4 pb-1"> Empire Waist Dresses</div>
+                        <div class="d-flex align-content-center justify-content-center"> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> </div>
+                        <div class="price">$ 70.0</div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
+                        <div class="product"> <img src="https://images.pexels.com/photos/6311392/pexels-photo-6311392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+                            <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
+                                <li class="icon"><span class="fas fa-expand-arrows-alt"></span></li>
+                                <li class="icon mx-3"><span class="far fa-heart"></span></li>
+                                <li class="icon"><span class="fas fa-shopping-bag"></span></li>
+                            </ul>
+                        </div>
+                        <div class="title pt-4 pb-1">Pinafore Dresses</div>
+                        <div class="d-flex align-content-center justify-content-center"> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> </div>
+                        <div class="price">$ 60.0</div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
+                        <div class="product"> <img src="https://images.pexels.com/photos/54203/pexels-photo-54203.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+                            <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
+                                <li class="icon"><span class="fas fa-expand-arrows-alt"></span></li>
+                                <li class="icon mx-3"><span class="far fa-heart"></span></li>
+                                <li class="icon"><span class="fas fa-shopping-bag"></span></li>
+                            </ul>
+                        </div>
+                        <div class="tag bg-red">sale</div>
+                        <div class="title pt-4 pb-1">Winter Sweater</div>
+                        <div class="d-flex align-content-center justify-content-center"> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> </div>
+                        <div class="price">$ 60.0</div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
+                        <div class="product"> <img src="https://images.pexels.com/photos/6764040/pexels-photo-6764040.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+                            <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
+                                <li class="icon"><span class="fas fa-expand-arrows-alt"></span></li>
+                                <li class="icon mx-3"><span class="far fa-heart"></span></li>
+                                <li class="icon"><span class="fas fa-shopping-bag"></span></li>
+                            </ul>
+                        </div>
+                        <div class="tag bg-black">out of stock</div>
+                        <div class="title pt-4 pb-1">Denim Dresses</div>
+                        <div class="d-flex align-content-center justify-content-center"> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> </div>
+                        <div class="price">$ 55.0</div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
+                        <div class="product"> <img src="https://images.pexels.com/photos/914668/pexels-photo-914668.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+                            <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
+                                <li class="icon"><span class="fas fa-expand-arrows-alt"></span></li>
+                                <li class="icon mx-3"><span class="far fa-heart"></span></li>
+                                <li class="icon"><span class="fas fa-shopping-bag"></span></li>
+                            </ul>
+                        </div>
+                        <div class="title pt-4 pb-1"> Empire Waist Dresses</div>
+                        <div class="d-flex align-content-center justify-content-center"> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> </div>
+                        <div class="price">$ 70.0</div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
+                        <div class="product"> <img src="https://images.pexels.com/photos/6311392/pexels-photo-6311392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+                            <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
+                                <li class="icon"><span class="fas fa-expand-arrows-alt"></span></li>
+                                <li class="icon mx-3"><span class="far fa-heart"></span></li>
+                                <li class="icon"><span class="fas fa-shopping-bag"></span></li>
+                            </ul>
+                        </div>
+                        <div class="tag bg-green">new</div>
+                        <div class="title pt-4 pb-1">Pinafore Dresses</div>
+                        <div class="d-flex align-content-center justify-content-center"> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> <span class="fas fa-star"></span> </div>
+                        <div class="price">$ 60.0</div>
+                    </div>
+                </div>
             </div>
         </>
     )
