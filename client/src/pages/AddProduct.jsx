6e83a853236313d0e2ec95b7e4cb0ea7242111e1
Gokul Hans V <gokulhansv@gmail.com>
2022-01-16 11:42:21 +0530

    halonew

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -1,46 +1,63 @@
-import React,{useState} from 'react'
-import {useNavigate} from 'react-router-dom'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 const AddProduct = () => {
 
     const navigate = useNavigate()
 
-   
+
     const [item, setitem] = useState('');
     const [price, setprice] = useState('');
     const [type, settype] = useState('');
     const [des, setdes] = useState('');
 
 
-    const PostData = ()=>{ 
+    const PostData = () => {
         console.log('çlicked');
-        fetch("products/add-product",{
-            method:"post",
-            headers:{
-                "Content-Type":"application/json"
+        fetch("products/add-product", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
             },
-            body:JSON.stringify({
+            body: JSON.stringify({
                 type,
-                item,price,des
+                item, price, des
             })
-        }).then(response=>response.json())
-        .then(data=>{
-            console.log(data);
-            navigate('/')
-        })
+        }).then(response => response.json())
+            .then(data => {
+                console.log(data);
+                navigate('/')
+            })
     }
 
 
 
     return (
-        <div>
-            <input type="text" placeholder='item' onChange={(e)=>setitem(e.target.value)} />
-            <input type="text" placeholder='type' onChange={(e)=>settype(e.target.value)} />
-            <input type="text" placeholder='description' onChange={(e)=>setdes(e.target.value)} />
-            <input type="text" placeholder='price' onChange={(e)=>setprice(e.target.value)} />
-            <input type="button" value="Add Product" onClick={()=>PostData()} />
-        </div>
+        // <div>
+        //     <input type="text" placeholder='item' onChange={(e)=>setitem(e.target.value)} />
+        //     <input type="text" placeholder='type' onChange={(e)=>settype(e.target.value)} />
+        //     <input type="text" placeholder='description' onChange={(e)=>setdes(e.target.value)} />
+        //     <input type="text" placeholder='price' onChange={(e)=>setprice(e.target.value)} />
+        //     <input type="button" value="Add Product" onClick={()=>PostData()} />
+        // </div>
+<div className="main">
+            
+        <form className="addproduct">
+            <div class="form-group">
+                <label for="email">Email address:</label>
+                <input type="email" class="form-control" id="email" />
+            </div>
+            <div class="form-group">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" id="pwd" />
+            </div>
+            <div class="checkbox">
+                <label><input type="checkbox" /> Remember me</label>
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+</div>
     )
 }
 
