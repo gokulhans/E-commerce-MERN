6e9fdb93e2f6069e92ffacc33bb9fd8e7a67d80b
Gokul Hans V <gokulhansv@gmail.com>
2022-01-17 19:40:19 +0530

    hmm

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ var db = require('./connection');
 app.use(express.json())
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 // getting and posting methods
 
