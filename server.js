324331c9f6cb85048a0280e38328d96a55ef0ccd
govindhansv <govindhansv333@gmail.com>
2022-01-17 17:51:03 +0530

    f

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var db = require('./connection');
 app.use(express.json())
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
