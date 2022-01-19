0810c06736c1a6c6e30195746541e10baa78bd2e
govindhansv <govindhansv333@gmail.com>
2022-01-19 12:58:18 +0530

    deploy

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-if(process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const express = require('express');
 const app = express();
@@ -25,8 +25,8 @@ app.use(cors());
 
 // getting and posting methods
 
-app.get('/',(req,res)=>{
-    res.send('Welcome to Backend! working')
+app.get('/', (req, res) => {
+  res.send('Welcome to Backend! working')
 })
 
 app.post("/post", (req, res) => {
@@ -43,30 +43,39 @@ const oneDay = 1000 * 60 * 60 * 24;
 //session middleware
 app.use(sessions({
   secret: "thisismysecertkeyfhrgfgrfrty84fwir767",
-    saveUninitialized:true,
-    cookie: { maxAge: oneDay },
-    resave: false
-  }));
+  saveUninitialized: true,
+  cookie: { maxAge: oneDay },
+  resave: false
+}));
 
 // router setting
 
-  var usersRouter = require('./routes/users');
-  var productsRouter = require('./routes/products');
-  var cartRouter = require('./routes/cart');
-  var shopRouter = require('./routes/shop');
+var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
+var cartRouter = require('./routes/cart');
+var shopRouter = require('./routes/shop');
 
-  app.use('/users',usersRouter);
-  app.use('/products',productsRouter)
-  app.use('/cart',cartRouter)
-  app.use('/shop',shopRouter)
+app.use('/users', usersRouter);
+app.use('/products', productsRouter)
+app.use('/cart', cartRouter)
+app.use('/shop', shopRouter)
 
-  // db connection
-  db.connect((err)=>{
-    if(err) console.log("Connection Error"+err);
-    else console.log("Database connected to port")
+// db connection
+db.connect((err) => {
+  if (err) console.log("Connection Error" + err);
+  else console.log("Database connected to port")
+})
+
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static('client/build'))
+  const path = require('path')
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
+}
 
 // server listening
-app.listen(process.env.PORT || PORT,()=>{
+app.listen(process.env.PORT || PORT, () => {
   console.log(`server is listening on ${PORT}`);
 })
