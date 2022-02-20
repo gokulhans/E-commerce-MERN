e01107bfa5f2fe172819f9f9c4193a058e4ef4ff
gokulhansv <94853880+gokulhans@users.noreply.github.com>
2022-02-20 10:43:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use(cors());
 
 // app.use(express.static(path.join(__dirname, 'client/build')));
 
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 // getting and posting methods
 
